2eb59b254f0e89ca992feed717ca017ffcf3c45e
Sean MacDonald <sean@gmail.com>
2016-02-20 18:45:49 -0800

    turns

--- testingRobot/testingRobot.ino
+++ testingRobot/testingRobot.ino
@@ -6,11 +6,13 @@ int M1 = 4;
 int E2 = 6;
 int M2 = 7;
 
-//Servo declarations 
-Servo myservo;  // create servo object to control a servo
-int ServoPin = 9; 
+int currentSpeed = 0;
 
-//temperature sensor and HC-SR05 declarations 
+//Servo declarations
+Servo myservo;  // create servo object to control a servo
+int ServoPin = 9;
+
+//temperature sensor and HC-SR05 declarations
 const int trigPin = 13;
 const int echoPin = 12;
 
@@ -26,32 +28,51 @@ float soundSpeed;
 int maxRange = 500;
 int minRange = 2;
 
-
+float dist;
+float leftDist;
+float rightDist;
 
 void setup()
 {
-  pinMode(10, OUTPUT); 
-  
+  pinMode(10, OUTPUT);
+
   //setup for wheels
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT);
 
-  //setup for servo 
+  //setup for servo
   myservo.attach(ServoPin);  // attaches the servo on pin 9 to the servo object
 
   //setup for distance sensor
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);  // write to trigger input for Sensor
   pinMode(echoPin, INPUT);  // read from echo output from Sensor
+  digitalWrite(10, HIGH);
 }
 
 void loop()
 {
-  digitalWrite(10, HIGH); 
-  sweep(1000, 15);
-  delay(2000); 
-  moveWheels();
+  accelerate(190, 4);
+  distance = getDistance();
+  Serial.println(distance);
+  if (distance < 30) {
+    accelerate(0, 8);
+    sweep(1000, 15);
+
+    if (leftDist < 30 && rightDist < 30) {
+      Serial.print("180");
+      turn(false, 1000, 250);
+      turn(false, 1000, 250);
+    }
+    else if (leftDist > rightDist) {
+      turn(false, 1000, 250);
+    }
+    else {
+        turn(true, 1000, 250);
+      }
+  }
 }
+
 
 
 //FUNCTIONS****************************************************************
@@ -64,24 +85,29 @@ void loop()
    parameter: turn_speed - used to control how fast the servo motors position changes.
 */
 void sweep(int wait_time, int turn_speed) {
-  int dist1, dist2; 
-  int pos; 
+  int dist1, dist2;
+  int pos;
   for (pos = 90; pos <= 180; pos += 1) {        // goes from 90 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);                         // tell servo to go to position in variable 'pos'
     delay(turn_speed);                         // waits turn_speed ms for the servo to reach the position
   }
-  delay(wait_time/2);                            //pauses for wait_time ms at position 180
-  dist1 = getDistance(); 
+  delay(wait_time / 2);                          //pauses for wait_time ms at position 180
+  leftDist = getDistance();
+  Serial.print("left: ");
+  Serial.print(leftDist);
+  delay(wait_time / 2);
 
   for (pos = 180; pos >= 0; pos -= 1) {         // goes from 180 degrees to 0 degrees
     myservo.write(pos);                          // tell servo to go to position in variable 'pos'
     delay(turn_speed);                           // waits turn_speed ms for the servo to reach the position
   }
-  delay(wait_time/2);                           //pauses for wait_time ms at position 0
-  dist2 = getDistance(); 
-  delay(wait_time/2);
-  
+  delay(wait_time / 2);                         //pauses for wait_time ms at position 0
+  rightDist = getDistance();
+  Serial.print("right: ");
+  Serial.println(rightDist);
+  delay(wait_time / 2);
+
   for (pos = 0; pos <= 90; pos += 1) {         // goes from 0 degrees to 90 degrees (back to the start position)
     // in steps of 1 degree
     myservo.write(pos);                       // tell servo to go to position in variable 'pos'
@@ -92,30 +118,20 @@ void sweep(int wait_time, int turn_speed) {
 /*
    Function - moveWheels - so far just for testing purposes
 */
-void moveWheels(void) {
-  int value;
-  for (value = 0 ; value <= 255; value += 5)
-  {
-    digitalWrite(M1, HIGH);
-    digitalWrite(M2, HIGH);
-    analogWrite(E1, value);   //PWM Speed Control
-    analogWrite(E2, value);   //PWM Speed Control
-    delay(30);
-  }
-   for (value = 255 ; value >= 0; value -= 5)
-  {
-    digitalWrite(M1, HIGH);
-    digitalWrite(M2, HIGH);
-    analogWrite(E1, value);   //PWM Speed Control
-    analogWrite(E2, value);   //PWM Speed Control
-    delay(30);
-  }
+void turn(boolean right, int time, int speed) {
+  digitalWrite(M1, right);
+  digitalWrite(M2, !right);
+  analogWrite(E1, speed);   //PWM Speed Control
+  analogWrite(E2, speed);   //PWM Speed Control
+  delay(time);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
 }
 
 /*
- * Function: getDistance - returns the distance measured  by the HC-SR05 and prints it onto the serial monitor
- */
-float getDistance(void){
+   Function: getDistance - returns the distance measured  by the HC-SR05 and prints it onto the serial monitor
+*/
+float getDistance(void) {
   // set up HC-SR04 by applying a 10 microsecond pulse to it
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -133,20 +149,37 @@ float getDistance(void){
 
   // reads the duration of the pulse when echoPin is high
   duration = pulseIn(echoPin, HIGH);
-  distance = duration / (2 * soundSpeed); // improve measurement precision
+  distance = duration / (2 * 29); // improve measurement precision (change soundspeed to 29)
 
-  if (distance <= maxRange && distance >= minRange) {
-    // print the result onto serial monitor
-      Serial.println(distance);
-      delay(500);        // delay for 500 ms to allow the echo fully fades before next reading
-      return distance; 
-  }
-  else {
-    Serial.println("ERROR: Object not detected.");
-    delay(500);           // delay for 500 ms to allow the echo fully fades before next reading
-    return NULL; 
-  }
-  
-  
+  return distance;
 }
+
+
+void accelerate(int finalSpeed, int speedChange) {
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+  if (finalSpeed > currentSpeed) {
+    for (currentSpeed;  currentSpeed <= finalSpeed; currentSpeed += speedChange) {
+      //Serial.println(currentSpeed);
+      analogWrite(E1, currentSpeed);   //PWM Speed Control
+      analogWrite(E2, currentSpeed);   //PWM Speed Control
+      delay(100);
+    }
+    analogWrite(E1, finalSpeed);   //PWM Speed Control
+    analogWrite(E2, finalSpeed);   //PWM Speed Control
+    delay(1);
+  } else {
+    for (currentSpeed;  currentSpeed >= finalSpeed; currentSpeed -= speedChange) {
+      //Serial.println(currentSpeed);
+      analogWrite(E1, currentSpeed);   //PWM Speed Control
+      analogWrite(E2, currentSpeed);   //PWM Speed Control
+      delay(100);
+    }
+    analogWrite(E1, finalSpeed);   //PWM Speed Control
+    analogWrite(E2, finalSpeed);   //PWM Speed Control
+    delay(1);
+  }
+}
+
+
 
