405343db11a1ae92010d673b23da39a57c492a9a
Alex <Alexmac22347@gmail.com>
2016-03-03 01:49:09 -0800

    panic

--- batteryRobot/batteryRobot.ino
+++ batteryRobot/batteryRobot.ino
@@ -11,10 +11,19 @@ const int ServoPin = 9;
 
 const int trigPin = 13;
 const int echoPin = 12;
+
 const int tempPin = A5;
+
+const int switchPin = 2;
+
+const int opticalPowerPin = 3;
+const int LOpticalPin = A4;
+const int MOpticalPin = A1;
+const int ROpticalPin = A0;
 
 //CONSTANTS:
 const int MAX_DISTANCE = 1000;
+const float RIGHT_WHEEL_CORRECT = .97;
 
 //GLOBAL VARIABLES:
 float distance;
@@ -32,99 +41,149 @@ void setup()
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
+
+  pinMode(opticalPowerPin, OUTPUT);
+
+  pinMode(switchPin, INPUT_PULLUP);
+
   turnServo(0);
   distance = getLowestDist(10); //bug fix/hack for distance = .1 initially
 }
 
 void loop() {
 
-  for (int i = 0; i < 4; i ++) {
-    changeSpeed(255, 4);
-    distance = getLowestDist(4);
-    if (distance < 40) {
-      avoidWall(40);
+
+  if (digitalRead(switchPin)) {
+    digitalWrite(opticalPowerPin, LOW);
+
+    for (int i = 0; i < 4; i ++) {
+      changeSpeed(255, 4);
+      distance = getLowestDist(4);
+      if (distance < 40) {
+        avoidWall(40);
+      }
+      delay(20);
     }
-    Serial.println(distance);
-    delay(50);
-  }
 
+    turnServo(70);
+    delay(20);
+    distance = getDistance();
 
-
-  turnServo(70);
-  delay(50);
-  distance = getDistance();
-  delay(100);
-
-  if (distance < 30) {
-    int adjust = 70;
-
-    float dist2 = getDistance();
-    //delta is the rate at which the wall is approaching. positive delta means wall is getting closer
-    float delta =  distance - dist2;
-    while (delta > .1 ) {
-      //slow/turn wheels
-      analogWrite(E1, 180);
-      analogWrite(E2, 80);
-
-      //get new delta
-      distance = getLowestDist(3);
+    if (distance < 35) {
       delay(100);
-      dist2 = getLowestDist(3);
-      delta =  distance - dist2;
+      int adjust = 70;
 
-      //also keep the servo facing the side wall
-      turnServo(adjust);
-      adjust += 2;
+      float dist2 = getDistance();
+      //delta is the rate at which the wall is approaching. positive delta means wall is getting closer
+      float delta =  distance - dist2;
+      while (delta > .1 ) {
+        //slow/turn wheels
+        analogWrite(E1, 180);
+        analogWrite(E2, 60);
+
+        //get new delta
+        distance = getLowestDist(3);
+        delay(100);
+        dist2 = getLowestDist(3);
+        delta =  distance - dist2;
+
+        //also keep the servo facing the side wall
+        turnServo(adjust);
+        adjust += 2;
+      }
     }
-  }
-  turnServo(0);
+    turnServo(0);
 
-  for (int i = 0; i < 4; i ++) {
-    changeSpeed(255, 4);
-    distance = getLowestDist(4);
-    if (distance < 40) {
-      avoidWall(40);
+    for (int i = 0; i < 4; i ++) {
+      changeSpeed(255, 4);
+      distance = getLowestDist(4);
+      if (distance < 40) {
+        avoidWall(40);
+      }
+
+      delay(20);
     }
-    Serial.println(distance);
-    delay(50);
-  }
 
-  turnServo(-70);
-  delay(50);
-  distance = getDistance();
-  delay(100);
+    turnServo(-70);
+    delay(20);
+    distance = getDistance();
 
-  if (distance < 30) {
-    int adjust = -70;
-
-    float dist2 = getDistance();
-    //delta is the rate at which the wall is approaching. positive delta means wall is getting closer
-    float delta =  distance - dist2;
-    while (delta > .1 ) {
-      //slow/turn wheels
-      analogWrite(E1, 80);
-      analogWrite(E2, 180);
-
-      //get new delta
-      distance = getLowestDist(3);
+    if (distance < 35) {
       delay(100);
-      dist2 = getLowestDist(3);
-      delta =  distance - dist2;
+      int adjust = -70;
 
-      //also keep the servo facing the side wall
-      turnServo(adjust);
-      adjust -= 2;
+      float dist2 = getDistance();
+      //delta is the rate at which the wall is approaching. positive delta means wall is getting closer
+      float delta =  distance - dist2;
+      while (delta > .1 ) {
+        //slow/turn wheels
+        analogWrite(E1, 60);
+        analogWrite(E2, 180);
+
+        //get new delta
+        distance = getLowestDist(3);
+        delay(100);
+        dist2 = getLowestDist(3);
+        delta =  distance - dist2;
+
+        //also keep the servo facing the side wall
+        turnServo(adjust);
+        adjust -= 2;
+      }
     }
+    turnServo(0);
   }
-  turnServo(0);
+  else {
+    digitalWrite(opticalPowerPin, HIGH);
+    blackLine(175);
+  }
 }
 
 //**************HIGHLEVELFUNCTIONS******************//
+void blackLine(int sensorThreshold) {
+  digitalWrite(M1, HIGH);
+  digitalWrite(M2, HIGH);
+
+  int LsensorValue;
+  int MsensorValue;
+  int RsensorValue;
+
+  // read the input on analog pin 0:
+  LsensorValue = analogRead(LOpticalPin);
+  delay(1);
+  //MsensorValue = analogRead(MOpticalPin);
+  //delay(1);
+  RsensorValue = analogRead(ROpticalPin);
+  delay(1);
+
+  Serial.print(LsensorValue);
+  Serial.print("  ");
+  Serial.println(RsensorValue);
+
+  if (LsensorValue <= sensorThreshold && RsensorValue <= sensorThreshold ) {
+    analogWrite(E1, 120);
+    analogWrite(E2, 120);
+  }
+
+  // Case Steer Left - as long as left sensor detects black line
+  else if (LsensorValue > sensorThreshold) {
+    analogWrite(E1, 10);
+    analogWrite(E2, 100);
+  }
+
+
+  // Case Steer Right - as long as right sensor detects black line
+  else if (RsensorValue > sensorThreshold) {
+    analogWrite(E1, 100);
+    analogWrite(E2, 10);
+  }
+
+}
 /*
    Checks to see if its going to crash into a wall and take corrective action
 */
 void avoidWall(int distThreshold) {
-  slowDown(10);
+  slowDown(5);
 
   //changeSpeed(0, 2);
   turnServo(90);
@@ -139,14 +198,14 @@ void avoidWall(int distThreshold) {
   turnServo(0);
 
   if (leftDist < distThreshold && rightDist < distThreshold) {
-    turn(true, 250, 235);
-    turn(true, 250, 235);
+    turn(true, 1000, 100);
+    turn(true, 1000, 100);
   }
   else if (leftDist > rightDist) {
-    turn(true, 250, 235);
+    turn(true, 1000, 100);
   }
   else {
-    turn(false, 250, 235);
+    turn(false, 1000, 100);
   }
 
 }
@@ -176,6 +235,7 @@ float getDistance(void) {
   float temperature;
   float thisDistance;
 
+  //this temperature varies from 25 to 32
   temperature = (5.0 * analogRead(tempPin) * 100.0) / 1024;
 
   // send pulse
@@ -186,11 +246,12 @@ float getDistance(void) {
   digitalWrite(trigPin, LOW);
 
   //receive pulse. Third argument is timout in microseconds
-  duration = pulseIn(echoPin, HIGH, 100000);
+  duration = pulseIn(echoPin, HIGH, 10000);
   if (duration == 0) {
     return MAX_DISTANCE;
   }
-  thisDistance  = (331.5 + (0.6 * temperature)) * duration / 2 * 100 / 1000000;
+  //thisDistance  = (331.5 + (0.6 * temperature)) * duration / 2 * 100 / 1000000;
+  thisDistance  = (331.5 + (0.6 * 25)) * duration / 2 * 100 / 1000000;
 
   return thisDistance;
 }
@@ -202,7 +263,7 @@ void changeSpeed(int finalSpeed, int acceleration) {
   if (finalSpeed > currentSpeed) {
     for (currentSpeed;  currentSpeed < finalSpeed; currentSpeed += acceleration) {
       analogWrite(E1, currentSpeed);
-      analogWrite(E2, currentSpeed);
+      analogWrite(E2, currentSpeed * (RIGHT_WHEEL_CORRECT));
       delay(20);
     }
     analogWrite(E1, finalSpeed);
@@ -211,11 +272,11 @@ void changeSpeed(int finalSpeed, int acceleration) {
     for (currentSpeed;  currentSpeed > finalSpeed; currentSpeed -= acceleration) {
 
       analogWrite(E1, currentSpeed);
-      analogWrite(E2, currentSpeed);
+      analogWrite(E2, currentSpeed * (RIGHT_WHEEL_CORRECT));
       delay(20);
     }
     analogWrite(E1, finalSpeed);
-    analogWrite(E2, finalSpeed);
+    analogWrite(E2, finalSpeed * (RIGHT_WHEEL_CORRECT));
   }
 }
 
